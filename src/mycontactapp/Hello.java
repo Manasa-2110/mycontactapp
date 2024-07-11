32690617215ecfd615797a6e84f871ff2c99d673
Manasa-2110 <indereshammanasa123@gmail.com>
2024-07-11 16:46:10 +0530

    Update Hello.java

--- src/mycontactapp/Hello.java
+++ src/mycontactapp/Hello.java
@@ -3,7 +3,9 @@ package mycontactapp;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello...everybody");
-		System.out.println("Hi...everyone");
+		 System.out.println("Hi...everyone");
+	        System.out.println("work");
+	        
 	}
 
 }
